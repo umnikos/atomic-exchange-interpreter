33c753e3d28f5a3b38ac7ae40adb0b2e3746c39a
umnikos <alex.stefanov2@gmail.com>
2022-05-23 19:20:48 +0300

    turn splitter and fibonacci into modules

--- fibonacci.py
+++ fibonacci.py
@@ -1,16 +1,23 @@
 from components import *
 
-one = NumberArrow((Zero(),0), 1)
-bottom = Intersection(None,(one,0))
-split = Splitter((bottom,0))
-zero = Zero()
-top = Intersection((split,0),(zero,0))
-for_output = Splitter((top,0))
-o = Output((for_output,1))
-arrow = WhiteArrow((split,1),(for_output,0))
-Disposal((arrow,0))
-sum = Sum((arrow,1))
-bottom.insert_input(0,(sum,0))
+def fibonacci():
+    one = NumberArrow((Zero(),0), 1)
+    bottom = Intersection(None,(one,0))
+    split = Splitter((bottom,0))
+    zero = Zero()
+    top = Intersection((split,0),(zero,0))
+    for_output = Splitter((top,0))
+    o = Output((for_output,1))
+    arrow = WhiteArrow((split,1),(for_output,0))
+    Disposal((arrow,0))
+    sum = Sum((arrow,1))
+    bottom.insert_input(0,(sum,0))
 
-e = IOEnclosure(o)
-e.run()
+    return Enclosure([],1,[],[o])
+
+if __name__ == "__main__":
+    f = fibonacci()
+    o = Output((f,0))
+    e = IOEnclosure(o)
+    e.run()
+
